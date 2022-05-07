a5746d0c15b38357e46f8c75382986bb18d7cbab
Padmavathi Rekapalli <manivannan.chidambaram.qk@fujifilm.com>
2022-05-07 21:18:15 +1000

    added changes to pom.xml

--- CBACodilityTask3API/scripts/InstallChrome.sh
+++ CBACodilityTask3API/scripts/InstallChrome.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
+sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
+sudo mv /usr/lib/firefox/firefox /usr/lib/firefox/firefox_backup
+sudo ln -s /opt/firefox/firefox /usr/lib/firefox/firefox
